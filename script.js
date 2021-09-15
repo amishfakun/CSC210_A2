959044146958247ebf42c0e517682e34bef10422
Amish Fakun <amishfakun20@gmail.com>
2021-09-15 19:09:21 -0400

    shit works

--- script.js
+++ script.js
@@ -1,63 +1,33 @@
 var selections = {};
 var checkboxElems = document.querySelectorAll("input[type='checkbox']");
-var totalElem = document.getElementById("seats-total");
-var seatsElem = document.getElementById("selected-seats");
+var totalElem = document.getElementById("totalScore");
+
 
 for (var i = 0; i < checkboxElems.length; i++) {
-  checkboxElems[i].addEventListener("click", displayCheck);
+  checkboxElems[i].addEventListener("click", generateResult);
 }
 
-function displayCheck(e) {
-  if (e.target.checked) {
-    selections[e.target.id] = {
-      name: e.target.name,
-      value: e.target.value,
-    };
-  } else {
+  var numericTotal=0;
+  var octalSymbol = '';
 
-    selections[e.target.id] = {
-      name: '-',
-      value: 0,
-    };
+function generateResult() {
+  
+  if (document.getElementById("OR").checked == true) {numericTotal+=400; octalSymbol+='r';} else{octalSymbol+='-'}
+  if (document.getElementById("OW").checked == true) {numericTotal+=200; octalSymbol+='w';} else{octalSymbol+='-'}
+  if (document.getElementById("OE").checked == true) {numericTotal+=100; octalSymbol+='x';} else{octalSymbol+='-'}
+  
+  if (document.getElementById("GR").checked == true) {numericTotal+=40; octalSymbol+='r';} else{octalSymbol+='-'}
+  if (document.getElementById("GW").checked == true) {numericTotal+=20; octalSymbol+='w';} else{octalSymbol+='-'}
+  if (document.getElementById("GE").checked == true) {numericTotal+=10; octalSymbol+='x';} else{octalSymbol+='-'}
+  
+  if (document.getElementById("PR").checked == true) {numericTotal+=4; octalSymbol+='r';} else{octalSymbol+='-'}
+  if (document.getElementById("PW").checked == true) {numericTotal+=2; octalSymbol+='w';} else{octalSymbol+='-'}
+  if (document.getElementById("PE").checked == true) {numericTotal+=1; octalSymbol+='x';} else{octalSymbol+='-'}
     
-  }
+  document.getElementById("numericTotal").innerHTML = numericTotal;
+  document.getElementById("octalSymbol").innerHTML = octalSymbol;
+  result.style.display = "block";
+  numericTotal = 0;
+  octalSymbol = '';
 
-  var result = [];
-  var total = 0;
-
-  for (var key in selections) {
-    var listItem =
-      "<li>" + selections[key].name  + "</li>";
-    result.push(listItem);
-    total += parseInt(selections[key].value);
-  }
-
-  totalElem.innerText = total;
-  seatsElem.innerHTML = result.join("");
 }
-
-
-//    var chmodDecValue=0;
-//   var chmodSymbol = '';
-
-// function chmodResult() {
-  
-//   if (document.getElementById("OR").checked == true) {chmodDecValue+=400; chmodSymbol+='r';} else{chmodSymbol+='-'}
-//   if (document.getElementById("OW").checked == true) {chmodDecValue+=200; chmodSymbol+='w';} else{chmodSymbol+='-'}
-//   if (document.getElementById("OE").checked == true) {chmodDecValue+=100; chmodSymbol+='x';} else{chmodSymbol+='-'}
-  
-//   if (document.getElementById("GR").checked == true) {chmodDecValue+=40; chmodSymbol+='r';} else{chmodSymbol+='-'}
-//   if (document.getElementById("GW").checked == true) {chmodDecValue+=20; chmodSymbol+='w';} else{chmodSymbol+='-'}
-//   if (document.getElementById("GE").checked == true) {chmodDecValue+=10; chmodSymbol+='x';} else{chmodSymbol+='-'}
-  
-//   if (document.getElementById("PR").checked == true) {chmodDecValue+=4; chmodSymbol+='r';} else{chmodSymbol+='-'}
-//   if (document.getElementById("PW").checked == true) {chmodDecValue+=2; chmodSymbol+='w';} else{chmodSymbol+='-'}
-//   if (document.getElementById("PE").checked == true) {chmodDecValue+=1; chmodSymbol+='x';} else{chmodSymbol+='-'}
-    
-//   document.getElementById("chmodDecValue").innerHTML = chmodDecValue;
-//   document.getElementById("chmodSymbol").innerHTML = chmodSymbol;
-//   result.style.display = "block";
-//   chmodDecValue =0;
-//   chmodSymbol = '';
-
-// }
